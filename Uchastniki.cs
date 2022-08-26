17536fd304ae3c01a2720f791827c34525b99aa1
maksimbutakov59 <maksimbutakov59@gmail.com>
2022-08-26 17:16:00 +0500

    Добавьте файлы проекта.

---
+++ Uchastniki.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic
+
+
+namespace C_Sharp_2 
+{
+	class Uchastniki
+	{
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+
+		public int Age { get; set; }
+	
+	}
+}
